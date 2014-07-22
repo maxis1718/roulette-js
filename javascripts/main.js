55d4c396c4d76041c275cf59a3d3ed3c951e5903
Maxis <maxis1718@gmail.com>
2014-07-22 22:09:11 +0800

    Add round

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@ function rotateImage()
 	// 至少一圈，最多兩圈
 	var round = 360*(1+Math.random());
 	// 
-	var random = Math.random()*360;
+	var random = Math.round(Math.random()*360);
 	var direction = (Math.random() > 0.5 ? 1 : -1);
 
 	var deg = (current_deg + round + random) * direction;
